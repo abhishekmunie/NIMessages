2aeb47c27f56e044a274920e410afcafbc59522b
Abhishek Munie <abhishekmunie@yahoo.co.in>
2014-10-21 15:05:35 +0530

    Initial Commit

---
+++ NIMessages/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NIMessages
+//
+//  Created by Abhishek Munie on 21/10/14.
+//  Copyright (c) 2014 Abhishek Munie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
